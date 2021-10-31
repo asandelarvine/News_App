260741a329ed35c18f0f750de93ab77e24056af7
asandelarvine <larvine.asande@student.moringaschool.com>
2021-10-31 15:23:25 +0300

    this is where we'll store newly created news object

---
+++ app/request.py
@@ -0,0 +1,56 @@
+from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
+
+# Getting api key
+api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+
+
+    return news_results
+
+def process_results(news_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain movie details
+
+    Returns :
+        news_results: A list of movie objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        title = news_item.get('original_title')
+        overview = news_item.get('overview')
+        poster = news_item.get('poster_path')
+        vote_average = news_item.get('vote_average')
+        vote_count = news_item.get('vote_count')
+
+        if poster:
+            news_object = News(id,title,overview,poster,vote_average,vote_count)
+            news_results.append(news_object)
+
+    return news_results
