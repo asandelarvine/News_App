c141c2dac9532f5cedad222d9002b610178321e7
asandelarvine <larvine.asande@student.moringaschool.com>
2021-10-31 14:04:20 +0300

    content added

--- app/views.py
+++ app/views.py
@@ -9,8 +9,8 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
