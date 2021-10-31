164a8669714a9f636b7c857eecfde5c93adc2eb2
asandelarvine <larvine.asande@student.moringaschool.com>
2021-10-31 15:26:10 +0300

    significant changes made

--- app/config.py
+++ app/config.py
@@ -2,6 +2,12 @@ class Config:
     '''
     General configuration parent class
     '''
+    NEWS_API_BASE_URL ='https://api.thenewsdb.org/3/news/{}?api_key={}'
+
+class Config:
+    '''
+    General configuration parent class
+    '''
     pass
 
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+from .request import get_movies
+
 
 # Views
 @app.route('/')
@@ -19,4 +21,21 @@ def news(news_id):
     View news page function that returns the news details page and its data
     '''
     return render_template('news.html',id = news_id)
+
+
+
+from .request import get_movies
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular news
+    popular_news = get_news('popular')
+    print(popular_news)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title,popular = popular_news)
 
