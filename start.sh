393d8314586053024a0924b91d2e1912d12df2f4
asandelarvine <larvine.asande@student.moringaschool.com>
2021-11-02 16:55:52 -0800

    update files

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-export NEWS_API_KEY='92024ca524b9462782a214ac25c4'
+# export NEWS_API_KEY='92024ca524b9462782a214ac25c4'
+export NEWS_API_KEY='b12c190aa55944f28493f5bd1c574c62'
 export SECRET_KEY='1263'
 
-python3.9 manage.py server
+python3 manage.py server
